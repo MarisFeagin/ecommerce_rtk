47afa3f6984b1922018df8f262cb815aa808863a
Maris Feagin <mfeagin64@gmail.com>
2025-02-13 22:08:49 -0500

    display products in cart/fully interactivity

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,43 +1,41 @@
 import { createSlice } from '@reduxjs/toolkit';
-
 const initialState = {
     cartItems: [],
 };
-
 const CartSlice = createSlice({
-  name: 'cart',
-  initialState,
-  reducers: {
-      addItemToCart(state, action) {
-          const existingItem = state.cartItems.find(item.id === action.payload.id);
-          if (existingItem) {
-              existingItem.quantity += 1;
-          } else {
-              stae.cartItems.push({ ...action.payload, quantity: 1});
-          }
-      },
-      removeItemFromCart(state, action) {
-          state.cartItems = state.cartItems.filter(item => item.id !== action.payload);
-      },
-      clearCart(state) {
-          state.cartItems = [];
-      },
-      increaseItemQuantity(state, action) {
-          const itemToIncrease = state.cartItems.find(item => item.id === action.payload);
-          if (itemToIncrease) {
-              itemToIncrease.quantity += 1;
-          }
-      },
-      decreaseItemQuantity(state, action) {
-          const itemToDecrease = state.cartItems.find(item => item.id === action.payload);
-          if (itemToDecrease && itemToDecrease.quantity > 1) {
-              itemToDecrease.quantity -= 1;
-          }
-      },
-  }
+    name: 'cart',
+    initialState,
+    reducers: {
+        addItemToCart(state, action) {
+            const existingItem = state.cartItems.find(item => item.id === action.payload.id);
+            if (existingItem) {
+                existingItem.quantity += 1;
+            } else {
+                state.cartItems.push({ ...action.payload, quantity: 1 });
+            }
+        },
+        removeItemFromCart(state, action) {
+            state.cartItems = state.cartItems.filter(item => item.id !== action.payload);
+        },
+        clearCart(state) {
+            state.cartItems = [];
+        },
+        increaseItemQuantity(state, action) {
+            const itemToIncrease = state.cartItems.find(item => item.id === action.payload);
+            if (itemToIncrease) {
+                itemToIncrease.quantity += 1;
+            }
+        },
+        decreaseItemQuantity(state, action) {
+            const itemToDecrease = state.cartItems.find(item => item.id === action.payload);
+            if (itemToDecrease && itemToDecrease.quantity > 1) {
+                itemToDecrease.quantity -= 1;
+            }
+        },
 
+
+    }
 });
-
 export const {
     addItemToCart,
     removeItemFromCart,
@@ -46,5 +44,4 @@ export const {
     decreaseItemQuantity,
 } = CartSlice.actions;
 export default CartSlice.reducer;
-
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux'
 import store from './store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
   </React.StrictMode>,
 )
